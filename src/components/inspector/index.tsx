f7415f94400933a7eebed86f09d7a3f185930542
Johan Fagerberg <Johan.Fagerberg@rsyd.dk>
2022-04-06 12:10:02 +0200

    fix: don't error in inspector when nothing is selected

--- src/components/inspector/index.tsx
+++ src/components/inspector/index.tsx
@@ -8,7 +8,7 @@ type DOMInspectorProps = {
     data: { [k: string]: any }
 };
 const DOMInspector = ({ data: _data }: DOMInspectorProps) => {
-    const { children, ...data } = _data;
+    const { children, ...data } = _data || {};
     return <div className={[style.container, !data ? style["container--empty"] : ""].join(" ")}>
         {data
             ? Object.keys(data).map(key => {
